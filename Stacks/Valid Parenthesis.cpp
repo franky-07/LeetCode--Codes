854b0e3770d5e65b0e201256a693aaba392031cb
franky-07 <93547484+franky-07@users.noreply.github.com>
2021-11-02 00:30:26 +0530

    Create Valid Parenthesis.cpp

---
+++ Stacks/Valid Parenthesis.cpp
@@ -0,0 +1,26 @@
+/*
+*/
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> s1;
+        for(auto c:s){
+           if(c=='('||c=='{'||c=='['){
+               s1.push(c);
+           }
+            else if (s1.empty()){
+                return false;
+            }
+            else{
+                if((s1.top()=='['&&c==']') ||(s1.top()=='('&&c==')') ||                 (s1.top()=='{'&&c=='}')){
+                    s1.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(!s1.empty())return false;
+        return true;
+    }
+};
